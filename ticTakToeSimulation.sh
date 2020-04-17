47ccd6c5efb962184484e48c4337ab0417e68c22
arun kumar <alekhya0801@gmail.com>
2020-04-17 19:55:19 +0530

    Added feature to reset the board

---
+++ ticTakToeSimulation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#constants
+NUM_OF_ROWS=3
+NUM_OF_COLUMNS=3
+#variables
+declare -A board
+resetBoard(){
+	for ((i=0;i<$NUM_OF_ROWS;i++))
+	do
+		for ((j=0;j<$NUM_OF_COLUMNS;j++))
+		do
+			board[$i,$j]=.
+		done
+	done
+}
+
+printBoard(){
+	printf "%4s"
+	for ((i=0;i<$NUM_OF_COLUMNS;i++))
+	do
+    		printf "%2s" $i " "
+	done
+	echo
+	for ((i=0;i<$NUM_OF_ROWS;i++))
+	do
+		printf  "%2s" $i " "
+		for ((j=0;j<$NUM_OF_COLUMNS;j++))
+		do
+			printf "%2s" ${board[$i,$j]} " "
+		done
+		echo
+	done
+}
+
+resetBoard
+printBoard
