3c14ee9f8b2c0b532d0b1f7793193182cf7eaa19
arun kumar <alekhya0801@gmail.com>
2020-04-18 10:27:48 +0530

    Added feature to check game status

--- ticTakToeSimulation.sh
+++ ticTakToeSimulation.sh
@@ -3,19 +3,8 @@
 NUM_OF_ROWS=3
 NUM_OF_COLUMNS=3
 #variables
+numOfTurns=0
 declare -A board
-
-echo You are assigned with letter X
-player[1]=X
-player[2]=O
-
-toss=$((RANDOM%2))
-if [ $toss -eq 1 ]
-then
-	echo player 1 start the game
-else
-	echo player 2 start the game
-fi
 
 resetBoard(){
 	for ((i=0;i<$NUM_OF_ROWS;i++))
@@ -45,10 +34,133 @@ printBoard(){
 	done
 }
 
-resetBoard
-printBoard
+checkWinningConditions(){
+	#check row wise
+	for (( rows=0; rows<$NUM_OF_ROWS; rows++ ))
+	do
+		if [ ${board[$rows,0]} != "." ]
+		then
+			count=1
+			for (( columns=1; columns<$NUM_OF_COLUMNS; columns++ ))
+			do
+				if [ ${board[$rows,0]} == ${board[$rows,$columns]} ]
+				then
+					((count++))
+				fi
+			done
+			if [ $count = $NUM_OF_COLUMNS ]
+			then
+				echo Game over player $player won
+				printBoard
+				exit;
+			fi
+		fi
+	done
 
-echo Choose your cell
-read -p "Enter row number " row
-read -p "Enter column number " column
-board[$row,$column]=X
+	#check column wise
+
+        for (( columns=0; columns<$NUM_OF_COLUMNS; columns++ ))
+        do
+                if [ ${board[0,$columns]} != "." ]
+                then
+                        count=1
+                        for (( rows=1; rows<$NUM_OF_ROWS; rows++ ))
+                        do
+                                if [ ${board[0,$columns]} = ${board[$rows,$columns]} ]
+                                then
+					((count++))
+				fi
+                        done
+                	if [ $count -eq $NUM_OF_ROWS ]
+                	then
+				echo Game over player $player won
+                        	printBoard
+				exit;
+                	fi
+		fi
+        done
+	#if board is square matrix then check diagonal elements
+	if [ $NUM_OF_ROWS = $NUM_OF_COLUMNS ]
+	then
+		#check diagonal from top left to bottom right
+		if [ ${board[0,0]} != "." ]
+                then
+			count=1
+			for (( rows=1; rows<$NUM_OF_ROWS; rows++ ))
+			do
+                               	if [ ${board[0,0]} == ${board[$rows,$rows]} ]
+				then
+                               		((count++))
+				fi
+			done
+                	if [ $count -eq $NUM_OF_ROWS ]
+                	then
+				echo Game over player $player won
+				printBoard
+				exit;
+                	fi
+        	fi
+
+		#check diagonal from top right to bottom left
+                columns1=$(($NUM_OF_COLUMNS-1))
+		columns2=$(($NUM_OF_COLUMNS-2))
+		if [ ${board[0,$columns1]} != "." ]
+                then
+			count=1
+			for (( rows=1; rows<$NUM_OF_ROWS; rows++ ))
+                	do
+                                if [ ${board[0,$columns1]} = ${board[$rows,$columns2]} ]
+                                then
+                                        ((count++))
+                                fi
+				columns2=$(($columns2-1))
+			done
+                        if [ $count -eq $NUM_OF_ROWS ]
+                        then
+				echo Game over player $player won
+                                printBoard
+				exit;
+                        fi
+		fi
+	fi
+
+	#condition for draw match
+	if [ $numOfTurns = $(($NUM_OF_ROWS*$NUM_OF_COLUMNS)) ]
+	then
+		echo Game over ..... Draw match
+		exit;
+	fi
+
+}
+
+chooseCell(){
+	printBoard
+	echo Choose your cell
+	read -p "Enter row number " row
+	read -p "Enter column number " column
+	if [ ${board[$row,$column]} = "." ]
+	then
+		board[$row,$column]=$1
+	else
+		echo invalid choice
+		chooseCell $symbol
+	fi
+
+}
+
+resetBoard
+echo You are player 1 assigned with letter X
+letter[1]=X
+letter[2]=O
+echo ${symbol[1]}
+toss=$((RANDOM%2+1))
+player=$toss
+while true
+do
+	symbol=${letter[$player]}
+	echo player $player turn with symbol $symbol
+	numOfTurns=$(($numOfTurns+1))
+	chooseCell $symbol
+	checkWinningConditions
+
+done
